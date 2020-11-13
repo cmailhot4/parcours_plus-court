0e24420a4f911bbfbb70a98ee6e643b6bc1034f2
cmailhot4 <mailhot.christophe@carrefour.cegepvicto.ca>
2020-11-12 19:36:31 -0500

    Chemin plus court fonctionnel

--- scripts/case.gd
+++ scripts/case.gd
@@ -1,19 +1,25 @@
 extends Node2D
 
 onready var case = $case
+var index
 var couleur
 var visite
 var type
 var voisins
 var voisins_accessibles
+var voisins_pivots
+var distance
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	index = null
 	couleur = Color(1, 1, 1, 1)
 	visite = false
 	type = null
 	voisins = []
 	voisins_accessibles = []
+	voisins_pivots = []
+	distance = 99999 #infini (relatif)
 
 # Permet de modifier les voisins de la case
 # v: tableau des voisins [gauche, haut, droite, bas]
@@ -21,10 +27,17 @@ func _set_voisins(v):
 	voisins = v
 	_set_voisins_accessibles()
 
+# Permet de setter les voisins qui sont des cases accessibles
 func _set_voisins_accessibles():
 	for i in range(voisins.size()):
 		if typeof(voisins[i]) == TYPE_INT:
 			voisins_accessibles.append(voisins[i])
+
+# Permet d'ajouter un voisin pivot avec la distance à celui-ci
+# v: index de la case pivot voisine
+# d: distance entre la case et v
+func _set_voisins_pivots(v, d):
+	voisins_pivots.append([v, d])
 
 # Change le type de la case et associe la bonne couleur
 func _set_type(t):
@@ -50,3 +63,13 @@ func _set_couleur(c):
 # Marque la case comme visitée
 func _set_visite():
 	visite = true
+
+# Change la valeur de la case (pour les cases pivots)
+# d: distance
+func _set_distance(d):
+	distance = d
+
+# Change la valeur de l'index de la case
+# i: index de la case (dans le tableau des cases)
+func _set_index(i):
+	index = i
--- scripts/main.gd
+++ scripts/main.gd
@@ -8,6 +8,8 @@ var nb_colonnes #nombre de colonnes dans le labyrinthe
 var i_depart #index de la case départ
 var i_arrivee #index de la case d'arrivée
 var ordre_parcours #tableau contenant les cases parcourrues en ordre
+var cases_pivots #tableau contenant les cases pivots du labyrinthe
+var cases_non_visitees #tableau des cases non visitées
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +17,11 @@ func _ready():
 	i_depart = 0
 	i_arrivee = 9
 	ordre_parcours = []
+	cases_pivots = []
+	cases_non_visitees = []
+	
+	# Set les indexs
+	_set_indexs()
 	
 	# Place les murs
 	_set_murs([1, 6, 8, 13, 16, 18])
@@ -25,9 +32,17 @@ func _ready():
 	# Initialise les voisins
 	_set_voisins()
 	
-	# Parcours le labyrinthe en profondeur jusqu'à temps que toutes les cases aient été visitées (continue même si on arrive à la fin)
-	_parcours_profondeur()
+	# Parcourt le labyrinthe pour trouver les cases pivots (celles qui ont 3 voisins accessibles).
+	# Servira à construire un graphe avec les cases pivots et leur poids (nombre de cases pour s'y rendre)
+	_trouver_cases_pivots()
 	
+	# Parcours le labyrinthe pour trouver le chemin le plus court de la case début à celle de fin
+	_parcours_plus_court()
+
+# Fonction qui met-à-jour la variable index de chaque case du labyrinthe
+func _set_indexs():
+	for i in range(cases.size()):
+		cases[i]._set_index(i)
 
 # Change le type des cases pour des murs aux index passés en paramètres
 # tab_index: tableau contenant les indexes des cases à transformer en murs
@@ -140,19 +155,114 @@ func _is_mur(c):
 	else:
 		return false
 
-# Fonction qui parcours le labyrinthe en profondeur et qui affiche la solution
-func _parcours_profondeur():
-	var parent = cases[i_depart]
+# Fonction qui parcourt le labyrinthe pour trouver les cases qui ont 3 voisins accessibles
+func _trouver_cases_pivots():
+	# l'entrée du labyrinthe est la 1ere case pivot
+	cases_pivots.append(cases[i_depart])
 	
-	_visiter(parent)
-	_afficher_solution()
+	# parcourt chaque case du labyrinthe et l'ajoute au tableau des cases pivots si la case n'est pas un mur, l'arrivée et qu'elle a au moins 3 voisins
+	for i in range(cases.size()):
+		if cases[i].type != 'mur':
+			if cases[i].type != 'arrivee':
+				if cases[i].voisins_accessibles.size() >= 3:
+					cases_pivots.append(cases[i])
+	
+	# l'arrivée du labyrinthe est la dernière case pivot
+	cases_pivots.append(cases[i_arrivee])
+	
+	# Voisins de 0
+	cases_pivots[0]._set_voisins_pivots(10, 2)
+	cases_pivots[0]._set_distance(0) #point de départ donc la distance est égale à 0
+	
+	# voisins de 10
+	cases_pivots[1]._set_voisins_pivots(0, 2)
+	cases_pivots[1]._set_voisins_pivots(12, 2)
+	cases_pivots[1]._set_voisins_pivots(22, 4)
+	
+	# voisins de 12
+	cases_pivots[2]._set_voisins_pivots(10, 2)
+	cases_pivots[2]._set_voisins_pivots(22, 2)
+	cases_pivots[2]._set_voisins_pivots(9, 5)
+	
+	# voisins de 22
+	cases_pivots[3]._set_voisins_pivots(10, 4)
+	cases_pivots[3]._set_voisins_pivots(12, 2)
+	cases_pivots[3]._set_voisins_pivots(9, 5)
+	
+	# voisins de 9
+	cases_pivots[4]._set_voisins_pivots(12, 5)
+	cases_pivots[4]._set_voisins_pivots(22, 5)
+
+# Fonction qui parcours le labyrinthe pour trouver le chemin le plus court
+func _parcours_plus_court():
+	# La case de départ (i_depart) possède une distance de 0
+	# Toutes les autres cases pivots possèdent un distance infinie (99999)
+	var case_actuelle = cases_pivots[0] #représente la case actuelle
+	var distance_min
+	var done = false
+	
+	# on ajoute les indexs des cases pivots au tableau des cases non visitées
+	for i in range(cases_pivots.size()):
+		cases_non_visitees.append(cases_pivots[i])
+	
+	while done == false:
+		# calcul la distance de chaque voisin de la case actuelle et trouve la prochaine case à visiter
+		distance_min = _visiter_voisins(case_actuelle)
+	
+		# vérifie que la case avec la plus petite distance est la case d'arrivée du labyrinthe
+		if distance_min == i_arrivee:
+			print(cases[distance_min].distance)
+			done = true
+		else:
+			case_actuelle = cases[distance_min]
+	
+	#_afficher_solution()
 	
 	print("Terminé")
+
+# Fonction qui permet de visiter les voisins d'une case pour changer leur valeur (distance)
+# c: case actuelle (celle dont on veut visiter les voisins)
+# return: index du voisin ayant la plus petite distance
+func _visiter_voisins(c):
+	var case_pivot_voisin
+	var dist
+	var distance_min = 100000
+	var i_next_case = -1
+	
+	# pour chaque voisin de la case actuelle
+	for i in range(c.voisins_pivots.size()):
+		var index_case_voisin = c.voisins_pivots[i][0]
+		case_pivot_voisin = cases[index_case_voisin] #case voisine de la case actuelle
+		
+		# calcul de la distance entre la case actuelle et sa case voisine
+		dist = c.distance + c.voisins_pivots[i][1]
+		
+		# vérifie si la distance entre la case actuelle et la case voisine est plus petite que la valeur de la case voisine
+		if dist < case_pivot_voisin.distance:
+			# la nouvelle distance est plus petite que l'ancienne donc on la change
+			case_pivot_voisin._set_distance(dist)
+	
+	# marquer la case actuelle comme visitée
+	c._set_visite()
+	
+	# enlève cette case du tableau des cases non visitées
+	cases_non_visitees.erase(c)
+	
+	# on cherche la case avec la plus petite distance dans le tableau des cases non visitées
+	for i in range(cases_non_visitees.size()):
+		# si la distance de la case non visitée est plus petite que la distance minimum
+		if cases_non_visitees[i].distance < distance_min:
+			# la case actuelle deviendra la case avec la plus petite distance du tableau des cases non visitées
+			i_next_case = cases_non_visitees[i].index
+			# la nouvelle distance minimum est la distance de la case à l'index i du tableau des cases non visitées
+			distance_min = cases_non_visitees[i].distance
+	
+	return i_next_case
 
 # Fonction récursive qui visite et marque les cases
 # case: la case que l'on visite
 func _visiter(case):
-	ordre_parcours.append(case)
+	#ordre_parcours.append(case)
 	case._set_visite()
 	
 	for i in range(case.voisins_accessibles.size()):
